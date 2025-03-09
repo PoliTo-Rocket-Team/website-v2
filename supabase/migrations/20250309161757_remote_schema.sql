e704bfb1de45eca51c3d844580b50b1bf64e2042
Huseyin Gulec <itshuseyingulec@gmail.com>
2025-03-09 17:20:02 +0100

    chore(db): add default values for timestamptz columns

--- supabase/migrations/20250309161757_remote_schema.sql
+++ supabase/migrations/20250309161757_remote_schema.sql
@@ -10,12 +10,22 @@ alter table "public"."roles" alter column "type" set default 'core_member'::"pos
 
 drop type "public"."position type__old_version_to_be_dropped";
 
+alter table "public"."applications" alter column "applied_at" set default (now() AT TIME ZONE 'utc'::text);
+
 alter table "public"."applications" alter column "applied_at" set not null;
 
 alter table "public"."applications" alter column "applied_at" set data type timestamp with time zone using "applied_at"::timestamp with time zone;
 
+alter table "public"."apply_positions" alter column "created_at" set default (now() AT TIME ZONE 'utc'::text);
+
+alter table "public"."members" alter column "nda_signed_at" set default (now() AT TIME ZONE 'utc'::text);
+
 alter table "public"."members" alter column "nda_signed_at" set data type timestamp with time zone using "nda_signed_at"::timestamp with time zone;
 
+alter table "public"."orders" alter column "created_at" set default (now() AT TIME ZONE 'utc'::text);
+
 alter table "public"."orders" alter column "created_at" set data type timestamp with time zone using "created_at"::timestamp with time zone;
+
+alter table "public"."users" alter column "created_at" set default (now() AT TIME ZONE 'utc'::text);
 
 
