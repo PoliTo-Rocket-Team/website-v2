8a604cdd83155d8e1be60a979f42df63fe70a45a
mehdiMansouri <pioneering.meti@gmail.com>
2025-04-22 22:38:56 +0200

    remove gap and etc to have full height and widht for child component

--- app/layout.tsx
+++ app/layout.tsx
@@ -35,8 +35,8 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
-          <main className="min-h-screen flex flex-col items-center">
-            <div className="flex-1 w-full flex flex-col gap-20 items-center">
+          <main className="min-h-screen flex flex-col items-center ">
+            <div className="flex-1 w-full flex flex-col items-center ">
               <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16 items-center">
                 <div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
                   <div className="flex gap-5 items-center font-bold">
@@ -46,11 +46,10 @@ export default function RootLayout({
                 </div>
                 <ThemeSwitcher />
               </nav>
-              <div className="flex flex-col gap-20 max-w-5xl p-5">
-                {children}
-              </div>
 
-              <footer className="w-full flex items-center justify-center border-t mx-auto text-center text-xs gap-8 py-16">
+              {children}
+
+              <footer className=" bg-red-500   w-full flex items-center justify-center border-t mx-auto text-center text-xs gap-8 py-16">
                 <p>Copyright text here</p>
               </footer>
             </div>
