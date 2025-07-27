9c676f374886b29c83d8a1e8cbd06e2b7c45ebad
Huseyin Gulec <itshuseyingulec@gmail.com>
2025-07-27 18:35:32 +0200

    fix(auth): add redirect function

--- components/signin-google.tsx
+++ components/signin-google.tsx
@@ -1,33 +1,21 @@
 "use client";
 
-import { useSearchParams } from "next/navigation";
 import { signIn } from "next-auth/react";
 import { useState, useEffect } from "react";
 
-import { cn } from "@/lib/utils";
-import { Button, buttonVariants } from "@/components/ui/button";
+import { Button } from "@/components/ui/button";
 import { Icons } from "@/components/icons";
 
 export default function SignInWithGoogle() {
-  const searchParams = useSearchParams();
-  const [callbackUrl, setCallbackUrl] = useState("/dashboard");
   const [isGoogleLoading, setIsGoogleLoading] = useState(false);
-
-  useEffect(() => {
-    const url = searchParams.get("callbackUrl");
-    if (url) setCallbackUrl(url);
-  }, [searchParams]);
 
   const handleGoogleSignIn = () => {
     setIsGoogleLoading(true);
-    signIn("google", { callbackUrl });
+    signIn("google");
   };
 
   return (
-    <Button
-      onClick={handleGoogleSignIn}
-      disabled={isGoogleLoading}
-    >
+    <Button onClick={handleGoogleSignIn} disabled={isGoogleLoading}>
       {isGoogleLoading ? (
         <Icons.spinner className="mr-2 h-4 w-4 animate-spin" />
       ) : (
