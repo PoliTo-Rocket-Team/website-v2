d221f7d0f468717369653387ba5a5994ae6616ab
Huseyin Gulec <itshuseyingulec@gmail.com>
2026-04-30 22:56:05 +0200

    Remove unused files

--- types/next-auth.d.ts
+++
@@ -1,9 +0,0 @@
-import NextAuth, { type DefaultSession } from "next-auth";
-
-declare module "next-auth" {
-  interface Session {
-    userId?: string;
-    email?: string;
-    user: DefaultSession["user"];
-  }
-}
